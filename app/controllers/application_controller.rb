083c9bb560c08f87d95bc90aa0a9c0469852b8b0
Yusuke Mukai <opposite0302@gmail.com>
2017-03-30 23:11:04 +0900

    Require authenticate excepts user_token routes

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,8 @@
 class ApplicationController < ActionController::API
   include Knock::Authenticable
 
+  before_action :authenticate_user
+
   def unauthorized_entity(entity_name)
     render json: ['Unauthorized'], status: :unauthorized
   end
