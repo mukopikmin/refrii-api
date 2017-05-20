efc4edf4077261b7c8c7dd0257bc8c4fbcdb166c
Yusuke Mukai <opposite0302@gmail.com>
2017-05-20 11:10:26 +0900

    updated gemfile.lock

--- app/serializers/box_serializer.rb
+++ app/serializers/box_serializer.rb
@@ -1,7 +1,11 @@
 class BoxSerializer < ActiveModel::Serializer
-  attributes :id, :name, :notice, :created_at, :updated_at
+  attributes :id, :name, :notice, :created_at, :updated_at, :is_invited
 
   belongs_to :user
   has_many :foods
   has_many :invitations
+
+  def is_invited
+    current_user != object.user
+  end
 end
