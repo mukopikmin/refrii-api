c6281039dbcd0fd25e74fc8d540f4dd45f046a09
Yusuke Mukai <opposite0302@gmail.com>
2017-07-18 23:39:25 +0900

    serialize invited user

--- app/serializers/box_serializer.rb
+++ app/serializers/box_serializer.rb
@@ -1,12 +1,15 @@
 class BoxSerializer < ActiveModel::Serializer
-  attributes :id, :name, :notice, :image_url, :created_at, :updated_at, :is_invited
+  attributes :id, :name, :notice, :image_url, :created_at, :updated_at, :is_invited, :invited_users
 
   belongs_to :user
   has_many :foods
-  has_many :invitations
 
   def is_invited
     current_user != object.user
+  end
+
+  def invited_users
+    object.invitations.map(&:user)
   end
 
   def image_url
