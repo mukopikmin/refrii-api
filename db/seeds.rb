b3b33ca8a37899af1048ebcbc653b6c037b30c41
Yusuke Mukai <mukopikmin@gmail.com>
2018-12-18 21:22:57 +0900

    add invitations seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,21 +1,6 @@
 # frozen_string_literal: true
 
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
-
-# User.create(name: 'Test User',
-#             email: 'test@test.com',
-#             admin: true,
-#             password: 'password',
-#             password_confirmation: 'password')
-
 10.times do |i|
-  # password = FFaker::Internet.password
   User.create(name: FFaker::Name.name,
               email: "test#{i}@test.com",
               password: 'password',
@@ -25,7 +10,7 @@ end
 20.times do
   Box.create(name: FFaker::Book.title,
              notice: FFaker::Lorem.paragraph,
-             owner: User.find(rand(User.first.id..User.last.id)))
+             owner: User.find(rand(User.first.id .. User.last.id)))
 end
 
 User.all.each do |user|
@@ -33,6 +18,9 @@ User.all.each do |user|
     Unit.create(label: label,
                 user: user)
   end
+
+  Invitation.new(user: user,
+                 box: Box.find(rand(Box.first.id .. Box.last.id)))
 end
 
 100.times do
