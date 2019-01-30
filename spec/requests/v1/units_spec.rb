20ee922b2ab9e721c080971a81ad1485038e94b4
Yusuke Mukai <mukopikmin@gmail.com>
2019-01-30 23:40:07 +0900

    add request spec /units

--- spec/requests/v1/units_spec.rb
+++ spec/requests/v1/units_spec.rb
@@ -3,9 +3,7 @@
 require 'rails_helper'
 
 RSpec.describe 'Units', type: :request do
-  def token(user)
-    JsonWebToken.payload(user)[:jwt]
-  end
+  include Committee::Rails::Test::Methods
 
   let(:user1) { create(:user) }
   let(:user2) { create(:user) }
@@ -14,228 +12,194 @@ RSpec.describe 'Units', type: :request do
 
   describe 'GET /units' do
     context 'without authentication' do
+      subject { response.status }
+
       before { get v1_units_path }
 
-      it 'returns 401' do
-        expect(response).to have_http_status(:unauthorized)
-      end
+      it { is_expected.to eq(401) }
     end
 
     context 'with authentication' do
-      before do
-        get v1_units_path, headers: { authorization: "Bearer #{token(user1)}" }
-      end
+      before { get v1_units_path, headers: { authorization: "Bearer #{token(user1)}" } }
 
-      it 'returns 200' do
-        expect(response).to have_http_status(:ok)
-      end
+      it { assert_schema_conform }
     end
   end
 
   describe 'GET /units/:id' do
     context 'without authentication' do
+      subject { response.status }
+
       before { get v1_unit_path(unit1) }
 
-      it 'returns 401' do
-        expect(response).to have_http_status(:unauthorized)
-      end
+      it { is_expected.to eq(401) }
     end
 
     context 'with authentication' do
       context 'with own unit' do
-        before do
-          get v1_unit_path(unit1), headers: { authorization: "Bearer #{token(user1)}" }
-        end
+        before { get v1_unit_path(unit1), headers: { authorization: "Bearer #{token(user1)}" } }
 
-        it 'returns 200' do
-          expect(response).to have_http_status(:ok)
-        end
+        it { assert_schema_conform }
       end
 
       context 'with other\'s unit' do
-        before do
-          get v1_unit_path(unit2), headers: { authorization: "Bearer #{token(user1)}" }
-        end
+        subject { response.status }
 
-        it 'returns 404' do
-          expect(response).to have_http_status(:not_found)
-        end
+        before { get v1_unit_path(unit2), headers: { authorization: "Bearer #{token(user1)}" } }
+
+        it { is_expected.to eq(404) }
       end
     end
   end
 
   describe 'POST /units' do
     context 'without authentication' do
+      subject { response.status }
+
       let(:params) { attributes_for(:unit).merge!(unit_id: unit1.to_param) }
 
       before { post v1_units_path, params: params }
 
-      it 'returns 401' do
-        expect(response).to have_http_status(:unauthorized)
-      end
+      it { is_expected.to eq(401) }
     end
 
     context 'with authentication' do
       context 'with valid params' do
         let(:params) { attributes_for(:unit) }
 
-        before do
-          post v1_units_path, params: params, headers: { authorization: "Bearer #{token(user1)}" }
-        end
+        before { post v1_units_path, params: params, headers: { authorization: "Bearer #{token(user1)}" } }
 
-        it 'returns 201' do
-          expect(response).to have_http_status(:created)
-        end
+        it { assert_schema_conform }
       end
 
       context 'with no label unit' do
+        subject { response.status }
+
         let(:params) { attributes_for(:no_label_unit) }
 
-        before do
-          post v1_units_path, params: params, headers: { authorization: "Bearer #{token(user1)}" }
-        end
+        before { post v1_units_path, params: params, headers: { authorization: "Bearer #{token(user1)}" } }
 
-        it 'returns 400' do
-          expect(response).to have_http_status(:bad_request)
-        end
+        it { is_expected.to eq(400) }
       end
 
       context 'with existing label unit' do
+        subject { response.status }
+
         let(:params) { attributes_for(:unit).merge!(user_id: user1.id) }
 
-        before { Unit.create(params) }
-
         before do
+          Unit.create(params)
           post v1_units_path, params: params, headers: { authorization: "Bearer #{token(user1)}" }
         end
 
-        it 'returns 400' do
-          expect(response).to have_http_status(:bad_request)
-        end
+        it { is_expected.to eq(400) }
       end
     end
   end
 
   describe 'PUT /units/:id' do
     context 'without authentication' do
+      subject { response.status }
+
       let(:params) { attributes_for(:unit) }
 
       before { put v1_unit_path(unit1), params: params }
 
-      it 'returns 401' do
-        expect(response).to have_http_status(:unauthorized)
-      end
+      it { is_expected.to eq(401) }
     end
 
     context 'with authentication' do
       context 'with own unit' do
         let(:params) { attributes_for(:unit) }
 
-        before do
-          put v1_unit_path(unit1), params: params, headers: { authorization: "Bearer #{token(user1)}" }
-        end
+        before { put v1_unit_path(unit1), params: params, headers: { authorization: "Bearer #{token(user1)}" } }
 
-        it 'returns 200' do
-          expect(response).to have_http_status(:ok)
-        end
+        it { assert_schema_conform }
       end
 
       context 'with other\'s unit' do
+        subject { response.status }
+
         let(:params) { attributes_for(:unit) }
 
-        before do
-          put v1_unit_path(unit2), params: params, headers: { authorization: "Bearer #{token(user1)}" }
-        end
+        before { put v1_unit_path(unit2), params: params, headers: { authorization: "Bearer #{token(user1)}" } }
 
-        it 'returns 400' do
-          expect(response).to have_http_status(:bad_request)
-        end
+        it { is_expected.to eq(400) }
       end
 
       context 'with no label unit' do
+        subject { response.status }
+
         let(:params) { attributes_for(:no_label_unit) }
 
-        before do
-          put v1_unit_path(unit2), params: params, headers: { authorization: "Bearer #{token(user1)}" }
-        end
+        before { put v1_unit_path(unit2), params: params, headers: { authorization: "Bearer #{token(user1)}" } }
 
-        it 'returns 400' do
-          expect(response).to have_http_status(:bad_request)
-        end
+        it { is_expected.to eq(400) }
       end
 
       context 'with existing label unit' do
+        subject { response.status }
+
         let(:params) { attributes_for(:unit).merge!(user_id: user1.id) }
 
-        before { Unit.create(params) }
-
         before do
+          Unit.create(params)
           put v1_unit_path(unit1), params: params, headers: { authorization: "Bearer #{token(user1)}" }
         end
 
-        it 'returns 400' do
-          expect(response).to have_http_status(:bad_request)
-        end
+        it { is_expected.to eq(400) }
       end
 
       context 'without renaming label of unit' do
         let(:params) { attributes_for(:unit).merge!(user_id: user1.id) }
 
-        before do
-          put v1_unit_path(unit1), params: params, headers: { authorization: "Bearer #{token(user1)}" }
-        end
+        before { put v1_unit_path(unit1), params: params, headers: { authorization: "Bearer #{token(user1)}" } }
 
-        it 'returns 200' do
-          expect(response).to have_http_status(:ok)
-        end
+        it { assert_schema_conform }
       end
     end
   end
 
   describe 'DELETE /units/:id' do
     context 'without authentication' do
+      subject { response.status }
+
       before { delete v1_unit_path(unit1) }
 
-      it 'returns 401' do
-        expect(response).to have_http_status(:unauthorized)
-      end
+      it { is_expected.to eq(401) }
     end
 
     context 'with authentication' do
       context 'with own unit' do
         context 'with unit not referenced by foods' do
-          before do
-            delete v1_unit_path(unit1), headers: { authorization: "Bearer #{token(user1)}" }
-          end
+          before { delete v1_unit_path(unit1), headers: { authorization: "Bearer #{token(user1)}" } }
 
-          it 'returns 201' do
-            expect(response).to have_http_status(:no_content)
-          end
+          it { assert_schema_conform }
         end
 
         context 'with unit referenced by foods' do
+          subject { response.status }
+
           let(:box) { create(:box, owner: user1) }
 
-          before { create(:food, box: box, unit: unit1, created_user: user1, updated_user: user1) }
-
           before do
+            create(:food, box: box,
+                          unit: unit1,
+                          created_user: user1,
+                          updated_user: user1)
             delete v1_unit_path(unit1), headers: { authorization: "Bearer #{token(user1)}" }
           end
 
-          it 'returns 400' do
-            expect(response).to have_http_status(:bad_request)
-          end
+          it { is_expected.to eq(400) }
         end
       end
 
       context 'with other\'s unit' do
-        before do
-          delete v1_unit_path(unit2), headers: { authorization: "Bearer #{token(user1)}" }
-        end
+        subject { response.status }
 
-        it 'returns 400' do
-          expect(response).to have_http_status(:bad_request)
-        end
+        before { delete v1_unit_path(unit2), headers: { authorization: "Bearer #{token(user1)}" } }
+
+        it { is_expected.to eq(400) }
       end
     end
   end
