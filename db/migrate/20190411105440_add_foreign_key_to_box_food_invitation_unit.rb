db6d92486af5156009efded329839c9ce31e4e6e
Yusuke Mukai <mukopikmin@gmail.com>
2019-04-11 22:17:05 +0900

    added foreign key constraint

---
+++ db/migrate/20190411105440_add_foreign_key_to_box_food_invitation_unit.rb
@@ -0,0 +1,10 @@
+class AddForeignKeyToBoxFoodInvitationUnit < ActiveRecord::Migration[5.2]
+  def change
+    add_foreign_key :boxes, :users, column: :owner_id
+    add_foreign_key :foods, :boxes, column: :box_id
+    add_foreign_key :foods, :untis, column: :unit_id
+    add_foreign_key :invitations, :boxes, column: :box_id
+    add_foreign_key :invitations, :users, column: :user_id
+    add_foreign_key :units, :users, column: :user_id
+  end
+end
