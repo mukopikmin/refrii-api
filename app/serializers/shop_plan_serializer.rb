fcc4b66b77c0dbddc36fabd43759206ceaf5f36f
Yusuke Mukai <mukopikmin@gmail.com>
2019-05-02 23:21:15 +0900

    added shop plan view to show date

--- app/serializers/shop_plan_serializer.rb
+++ app/serializers/shop_plan_serializer.rb
@@ -4,6 +4,7 @@ class ShopPlanSerializer < ActiveModel::Serializer
   attributes :id,
              :notice,
              :done,
+             :date,
              :amount,
              :created_at,
              :updated_at
