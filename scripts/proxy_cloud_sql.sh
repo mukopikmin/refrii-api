67526ec200cd29d4bbf20ad9a91c8df40fda5dd6
Yusuke Mukai <mukopikmin@gmail.com>
2019-06-11 01:04:21 +0900

    use unixsocket without docker on local

--- scripts/proxy_cloud_sql.sh
+++ scripts/proxy_cloud_sql.sh
@@ -2,11 +2,16 @@
 
 set -ex
 
-docker run \
-  -v $PWD/credentials/credentials.staging.json:/config \
-  -p 127.0.0.1:3306:3306 \
-  gcr.io/cloudsql-docker/gce-proxy:1.12 \
-  /cloud_sql_proxy \
-    -instances refrii-169906:us-central1:refrii-api=tcp:0.0.0.0:3306 \
-    -credential_file /config
-    
+# docker run \
+#   -v $PWD/credentials/credentials.staging.json:/config \
+#   -p 127.0.0.1:3306:3306 \
+#   gcr.io/cloudsql-docker/gce-proxy:1.12 \
+#   /cloud_sql_proxy \
+#     -instances refrii-169906:us-central1:refrii-api=tcp:0.0.0.0:3306 \
+#     -credential_file /config
+
+./cloud_sql_proxy \
+  -dir /cloudsql \
+  -instances refrii-169906:us-central1:refrii-api \
+  -credential_file credentials/credentials.staging.json
+  
