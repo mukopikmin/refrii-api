223571b33e118e3bd8d3c34978c720130450b199
Yusuke Mukai <mukopikmin@gmail.com>
2019-08-25 22:49:22 +0900

    fix deploy script deploying to new environment

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -8,6 +8,6 @@ image=$2
 gcloud beta run deploy $service \
   --image $image \
   --allow-unauthenticated \
-  --region us-central1 \
+  --region asia-northeast1 \
   --platform managed \
   --memory 512Mi
--- scripts/proxy_cloud_sql.sh
+++ scripts/proxy_cloud_sql.sh
@@ -2,16 +2,20 @@
 
 set -ex
 
+project=refrii-169906
+region=asia-northeast1
+instance=refrii-api-mysql
+
 # docker run \
 #   -v $PWD/credentials/credentials.staging.json:/config \
 #   -p 127.0.0.1:3306:3306 \
 #   gcr.io/cloudsql-docker/gce-proxy:1.12 \
 #   /cloud_sql_proxy \
-#     -instances refrii-169906:us-central1:refrii-api=tcp:0.0.0.0:3306 \
+#     -instances ${project}:${region}:${instance}=tcp:0.0.0.0:3306 \
 #     -credential_file /config
 
 ./cloud_sql_proxy \
   -dir /cloudsql \
-  -instances refrii-169906:us-central1:refrii-api \
+  -instances ${project}:${region}:${instance} \
   -credential_file credentials/credentials.staging.json
   
