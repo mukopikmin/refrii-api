c7fde023c19b84454b175c9c094b59d0ae0ad1f4
Yusuke Mukai <opposite0302@gmail.com>
2019-12-04 18:20:25 +0900

    deploy with tag push

--- scripts/migrate.sh
+++ scripts/migrate.sh
@@ -2,8 +2,8 @@
 
 set -ex
 
-branch=$@
-instance=$(echo $MYSQL_SOCKET | sed s/\\/cloudsql\\///)
+target=$1
+sql_instance=$(echo $MYSQL_SOCKET | sed s/\\/cloudsql\\///)
 
 mkdir /cloudsql
 
@@ -11,5 +11,5 @@ wget https://dl.google.com/cloudsql/cloud_sql_proxy.linux.amd64
 mv cloud_sql_proxy.linux.amd64 cloud_sql_proxy
 chmod +x cloud_sql_proxy
 
-sh -c "./cloud_sql_proxy -dir=/cloudsql -instances $instance -credential_file /kms/credentials.$branch.json" & 
+sh -c "./cloud_sql_proxy -dir=/cloudsql -instances $sql_instance -credential_file /kms/credentials.$target.json" & 
 bundle exec rails db:migrate
