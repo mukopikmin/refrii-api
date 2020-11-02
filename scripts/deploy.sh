72a75befef0a86dd3d8f51a06204e084ab133e60
Yusuke Mukai <mukopikmin@gmail.com>
2020-11-03 00:00:47 +0900

    Set instance count

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -5,11 +5,14 @@ set -ex
 service=$1
 image=$2
 
-gcloud run deploy $service \
+gcloud beta run deploy $service \
   --image $image \
   --allow-unauthenticated \
   --region asia-northeast1 \
   --platform managed \
   --memory 1Gi \
+  --min-instances 1 \
+  --max-instances 5 \
   --labels env=production \
   --update-env-vars RELEASE_HASH=$RELEASE_HASH
+ 
